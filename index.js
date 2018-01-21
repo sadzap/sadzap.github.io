bccecb15e5e0b9d6756da806c6261da74d8225ac
ascordero <anjocordero@gmail.com>
2018-01-20 23:29:29 -0800

    read database

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 var database = firebase.database();
 var query = "/";
 var name = "Zakiah";
-// firebase.database().ref(query).once('value').then(function(snapshot)) {
-//     if (snapshot.val() === 1) {
-//
-//     }
-// }
+
 firebase.database().ref(query).update({ // query's position(root) in the database, key [name] gets value 510
     [name] : {
         age : 10,
         lvl : 5
     }
+})
+
+query = "Zakiah";
+
+firebase.database().ref(query).once('value').then(function(snapshot) {
+    if (snapshot.val()) {
+        console.log(snapshot.val());
+    }
+    else {
+        console.log("Zakiah is ded.");
+    }
+}).catch(error => {
+    console.log(error.message);
 })
