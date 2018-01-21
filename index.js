ea749e1a7874efe7acd333fcff25e0999f724e9b
ascordero <anjocordero@gmail.com>
2018-01-21 01:38:52 -0800

    database is more concise

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var database = firebase.database();
 var query = "/characters/"; // root
 var name = "Zakiah";
 
-firebase.database().ref(query).update({ // query's position(root) in the database, key [name] gets value 510
+database.ref(query).update({ // query's position(root) in the database, key [name] gets value 510
     [name] : {
         age : 10,
         lvl : 5
@@ -21,7 +21,7 @@ database.ref(query).update({
 name = "Your character";
 query = "Kenyu Diggitt";
 
-firebase.database().ref(query).once('value').then(function(snapshot) {
+database.ref(query).once('value').then(function(snapshot) {
     if (snapshot.val()) {
         console.log(snapshot.val());
     }
